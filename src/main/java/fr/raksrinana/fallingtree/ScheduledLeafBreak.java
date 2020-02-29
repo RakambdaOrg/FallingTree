324d6c388e9f3b915501f3a08f25f6c02216044f
Thomas Couchoud <thomas.couchoud@gmail.com>
2020-02-29 21:22:44 +0100

    v2.0.4+21

---
+++ src/main/java/fr/raksrinana/fallingtree/ScheduledLeafBreak.java
@@ -0,0 +1,8 @@
+package fr.raksrinana.fallingtree;
+
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IWorld;
+
+public class ScheduledLeafBreak{
+	public ScheduledLeafBreak(IWorld world, BlockPos neighborPos){}
+}
