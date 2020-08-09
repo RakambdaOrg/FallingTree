a028154424497efe49bd6805fb3cff14bee67b32
Thomas Couchoud <thomas.couchoud@gmail.com>
2020-08-09 21:38:25 +0200

    Gradle stuff

--- src/main/java/fr/raksrinana/fallingtree/FallingTree.java
+++ src/main/java/fr/raksrinana/fallingtree/FallingTree.java
@@ -10,15 +10,12 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.network.FMLNetworkConstants;
 import net.minecraftforge.forgespi.language.IModInfo;
 import org.apache.commons.lang3.tuple.Pair;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.apache.maven.artifact.versioning.ArtifactVersion;
 import javax.annotation.Nonnull;
 
 @Mod(FallingTree.MOD_ID)
 public class FallingTree{
 	public static final String MOD_ID = "falling_tree";
-	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 	
 	public FallingTree(){
 		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
