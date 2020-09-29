0cd90df361cc1081c6dbf00c0c962c7536dc68e9
Thomas Couchoud <thomas.couchoud@gmail.com>
2020-09-29 21:36:39 +0200

    Shift down nether trees

--- src/main/java/fr/raksrinana/fallingtree/tree/Tree.java
+++ src/main/java/fr/raksrinana/fallingtree/tree/Tree.java
@@ -1,75 +1,57 @@
 package fr.raksrinana.fallingtree.tree;
 
-import fr.raksrinana.fallingtree.utils.TreePart;
+import fr.raksrinana.fallingtree.utils.TreePartType;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import java.util.*;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 public class Tree{
 	private final World world;
-	private final Set<BlockPos> logs;
-	private final Set<BlockPos> warts;
+	private final Set<TreePart> parts;
 	private final BlockPos hitPos;
 	
 	public Tree(World world, BlockPos blockPos){
 		this.world = world;
 		this.hitPos = blockPos;
-		this.logs = new LinkedHashSet<>();
-		this.warts = new LinkedHashSet<>();
+		this.parts = new LinkedHashSet<>();
 	}
 	
-	public void addLog(BlockPos blockPos){
-		if(Objects.nonNull(blockPos)){
-			this.logs.add(blockPos);
-		}
+	public void addPart(TreePart treePart){
+		this.parts.add(treePart);
 	}
 	
-	public void addWart(BlockPos blockPos){
-		if(Objects.nonNull(blockPos)){
-			this.warts.add(blockPos);
-		}
+	public Optional<TreePart> getLastSequencePart(){
+		return getParts().stream()
+				.max(Comparator.comparingInt(TreePart::getSequence));
 	}
 	
-	public void addPart(TreePart treePart, BlockPos blockPos){
-		switch(treePart){
-			case LOG:
-				addLog(blockPos);
-				break;
-			case WART:
-				addWart(blockPos);
-				break;
-		}
+	public Collection<TreePart> getWarts(){
+		return getParts().stream()
+				.filter(part -> part.getTreePartType() == TreePartType.WART)
+				.collect(Collectors.toSet());
 	}
 	
-	public Collection<BlockPos> getWarts(){
-		return this.warts;
-	}
-	
-	public Optional<BlockPos> getTopMostFurthestPart(){
-		return getTopMostPart().flatMap(topMost -> logs.stream()
-				.filter(log -> Objects.equals(log.getY(), topMost.getY()))
-				.max(Comparator.comparingInt(this::getDistanceFromHit)));
+	public Collection<TreePart> getLogs(){
+		return getParts().stream()
+				.filter(part -> part.getTreePartType() == TreePartType.LOG)
+				.collect(Collectors.toSet());
 	}
 	
 	private Optional<BlockPos> getTopMostPart(){
-		return getAllPartsStream().max(Comparator.comparingInt(BlockPos::getY));
+		return getParts().stream()
+				.map(TreePart::getBlockPos)
+				.max(Comparator.comparingInt(BlockPos::getY));
 	}
 	
 	public Optional<BlockPos> getTopMostLog(){
-		return getLogs().stream().max(Comparator.comparingInt(BlockPos::getY));
-	}
-	
-	private Stream<BlockPos> getAllPartsStream(){
-		return Stream.concat(getLogs().stream(), getWarts().stream());
-	}
-	
-	public int getDistanceFromHit(BlockPos pos){
-		return Math.abs(hitPos.getX() - pos.getX()) + Math.abs(hitPos.getY() - pos.getY()) + Math.abs(hitPos.getZ() - pos.getZ());
+		return getLogs().stream()
+				.map(TreePart::getBlockPos)
+				.max(Comparator.comparingInt(BlockPos::getY));
 	}
 	
 	public int getLogCount(){
-		return this.logs.size();
+		return this.getLogs().size();
 	}
 	
 	public BlockPos getHitPos(){
@@ -80,7 +62,7 @@ public class Tree{
 		return this.world;
 	}
 	
-	public Collection<BlockPos> getLogs(){
-		return this.logs;
+	public Collection<TreePart> getParts(){
+		return this.parts;
 	}
 }
