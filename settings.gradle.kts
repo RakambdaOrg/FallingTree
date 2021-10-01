bf3930ccf2db94ef299362a473f02fab46f49d66
Thomas Couchoud <thomas.couchoud@gmail.com>
2021-10-01 20:13:16 +0200

    1.18 port

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,21 +2,16 @@ enableFeaturePreview("VERSION_CATALOGS")
 
 pluginManagement {
     repositories {
-        jcenter()
         maven {
             name = "Fabric"
             url = uri("https://maven.fabricmc.net/")
         }
         gradlePluginPortal()
     }
-
-    val loom_version: String by settings
-    plugins {
-        id("fabric-loom").version(loom_version)
-    }
 }
 
 rootProject.name = "FallingTree"
 include("common")
 include("fabric")
-include("forge")
+//TODO: Renable for 1.18
+//include("forge")
