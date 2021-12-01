0920b88fac7e4db8875b131760b745a7b8f3bc57
Thomas Couchoud <thomas.couchoud@gmail.com>
2021-12-01 18:57:00 +0100

    1.18 Forge

--- forge/src/main/java/fr/raksrinana/fallingtree/forge/FallingTree.java
+++ forge/src/main/java/fr/raksrinana/fallingtree/forge/FallingTree.java
@@ -7,7 +7,6 @@ import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fmllegacy.network.FMLNetworkConstants;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import java.lang.reflect.InvocationTargetException;
@@ -16,9 +15,10 @@ import java.lang.reflect.InvocationTargetException;
 public class FallingTree{
 	public static final String MOD_ID = "fallingtree";
 	public static final Logger logger = LogManager.getLogger(MOD_ID);
+	public static final String IGNORESERVERONLY = "OHNOES\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31";
 	
 	public FallingTree(){
-		ModLoadingContext.get().registerExtensionPoint(DisplayTest.class, () -> new DisplayTest(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+		ModLoadingContext.get().registerExtensionPoint(DisplayTest.class, () -> new DisplayTest(() -> IGNORESERVERONLY, (a, b) -> true));
 		ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.COMMON_SPEC);
 		
 		if(ModList.get().isLoaded("cloth_config")){
