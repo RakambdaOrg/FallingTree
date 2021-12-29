8e57359b8ccf672fc59bf9c1ccad06cf1a848eb7
Thomas Couchoud <thomas.couchoud@gmail.com>
2021-12-29 19:18:40 +0100

    FallingTree is now required on both sides

--- forge/src/main/java/fr/raksrinana/fallingtree/forge/FallingTree.java
+++ forge/src/main/java/fr/raksrinana/fallingtree/forge/FallingTree.java
@@ -3,13 +3,11 @@ package fr.raksrinana.fallingtree.forge;
 import fr.raksrinana.fallingtree.forge.config.Config;
 import fr.raksrinana.fallingtree.forge.config.cloth.ClothConfigHook;
 import fr.raksrinana.fallingtree.forge.enchant.FallingTreeEnchantments;
-import net.minecraftforge.fml.IExtensionPoint.DisplayTest;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.network.NetworkConstants;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import java.lang.reflect.InvocationTargetException;
@@ -20,7 +18,6 @@ public class FallingTree{
 	public static final Logger logger = LogManager.getLogger(MOD_ID);
 	
 	public FallingTree(){
-		ModLoadingContext.get().registerExtensionPoint(DisplayTest.class, () -> new DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
 		ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.COMMON_SPEC);
 		
 		if(ModList.get().isLoaded("cloth_config")){
