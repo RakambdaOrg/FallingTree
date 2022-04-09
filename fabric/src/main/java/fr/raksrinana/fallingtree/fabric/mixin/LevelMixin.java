2ee6806a0a51f8eea158846dd6445803bf698db7
Thomas Couchoud <thomas.couchoud@gmail.com>
2022-04-09 10:05:25 +0200

    Fix leaves breaking not working

--- fabric/src/main/java/fr/raksrinana/fallingtree/fabric/mixin/LevelMixin.java
+++ fabric/src/main/java/fr/raksrinana/fallingtree/fabric/mixin/LevelMixin.java
@@ -8,7 +8,6 @@ import fr.raksrinana.fallingtree.fabric.common.wrapper.ServerLevelWrapper;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,34 +16,30 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.util.EnumSet;
 import java.util.stream.Collectors;
 
-@Mixin(Level.class)
+@Mixin(ServerLevel.class)
 public abstract class LevelMixin{
 	@Inject(method = "updateNeighborsAt", at = @At(value = "TAIL"))
 	public void updateNeighborsAlways(BlockPos pos, Block block, CallbackInfo callbackInfo){
 		//noinspection ConstantConditions
-		var level = (Level) (Object) this;
-		if(level instanceof ServerLevel serverLevel){
-			FallingTree.getMod().getLeafBreakingHandler().onBlockUpdate(
-					new ServerLevelWrapper(serverLevel),
-					new BlockPosWrapper(pos),
-					new BlockStateWrapper(level.getBlockState(pos)),
-					EnumSet.allOf(DirectionCompat.class));
-		}
+		var serverLevel = (ServerLevel) (Object) this;
+		FallingTree.getMod().getLeafBreakingHandler().onBlockUpdate(
+				new ServerLevelWrapper(serverLevel),
+				new BlockPosWrapper(pos),
+				new BlockStateWrapper(serverLevel.getBlockState(pos)),
+				EnumSet.allOf(DirectionCompat.class));
 	}
 	
 	@Inject(method = "updateNeighborsAtExceptFromFacing", at = @At(value = "TAIL"))
 	public void updateNeighborsExcept(BlockPos pos, Block sourceBlock, Direction direction, CallbackInfo callbackInfo){
 		//noinspection ConstantConditions
-		var level = (Level) (Object) this;
-		if(level instanceof ServerLevel serverLevel){
-			var directions = EnumSet.allOf(Direction.class);
-			directions.remove(direction);
-			
-			FallingTree.getMod().getLeafBreakingHandler().onBlockUpdate(
-					new ServerLevelWrapper(serverLevel),
-					new BlockPosWrapper(pos),
-					new BlockStateWrapper(level.getBlockState(pos)),
-					directions.stream().map(FallingTree.getMod()::asDirectionCompat).collect(Collectors.toSet()));
-		}
+		var serverLevel = (ServerLevel) (Object) this;
+		var directions = EnumSet.allOf(Direction.class);
+		directions.remove(direction);
+		
+		FallingTree.getMod().getLeafBreakingHandler().onBlockUpdate(
+				new ServerLevelWrapper(serverLevel),
+				new BlockPosWrapper(pos),
+				new BlockStateWrapper(serverLevel.getBlockState(pos)),
+				directions.stream().map(FallingTree.getMod()::asDirectionCompat).collect(Collectors.toSet()));
 	}
 }
