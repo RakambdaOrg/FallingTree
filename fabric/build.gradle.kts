25442b4a3891f95919e5bb781c89ca26232dc71a
Thomas Couchoud <zerderr@gmail.com>
2023-09-16 20:28:42 +0200

    Fix uuid

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -65,7 +65,7 @@ loom {
 
             property("fabric.log.level", "info")
             vmArg("-XX:+ShowCodeDetailsInExceptionMessages")
-            programArgs("--uuid=123", "--username=Dev")
+            programArgs("--uuid=f13e3278-dfb8-4948-98cc-7701b5c62e8c", "--username=Dev")
         }
         create("FTFabricServer") {
             server()
