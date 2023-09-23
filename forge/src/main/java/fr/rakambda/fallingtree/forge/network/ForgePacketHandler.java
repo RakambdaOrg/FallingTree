4e838275bfe891d8b2a7ba3301ea3b0060b2f2a2
Thomas Couchoud <zerderr@gmail.com>
2023-09-23 11:22:24 +0200

    Update forge

--- forge/src/main/java/fr/rakambda/fallingtree/forge/network/ForgePacketHandler.java
+++ forge/src/main/java/fr/rakambda/fallingtree/forge/network/ForgePacketHandler.java
@@ -4,38 +4,20 @@ import fr.rakambda.fallingtree.common.FallingTreeCommon;
 import fr.rakambda.fallingtree.common.network.ClientPacketHandler;
 import fr.rakambda.fallingtree.common.network.ConfigurationPacket;
 import fr.rakambda.fallingtree.common.network.ServerPacketHandler;
-import fr.rakambda.fallingtree.common.wrapper.IServerPlayer;
 import fr.rakambda.fallingtree.forge.FallingTree;
 import fr.rakambda.fallingtree.forge.common.wrapper.FriendlyByteBufWrapper;
 import lombok.RequiredArgsConstructor;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.network.CustomPayloadEvent;
 import net.minecraftforge.fml.DistExecutor;
-import net.minecraftforge.network.NetworkEvent;
-import net.minecraftforge.network.NetworkRegistry;
-import net.minecraftforge.network.PacketDistributor;
-import net.minecraftforge.network.simple.SimpleChannel;
-import org.jetbrains.annotations.NotNull;
-import java.util.Collection;
-import java.util.Set;
-import java.util.function.Supplier;
+import net.minecraftforge.network.SimpleChannel;
 
 @RequiredArgsConstructor
 public class ForgePacketHandler implements ClientPacketHandler, ServerPacketHandler{
-	private static final String PROTOCOL_VERSION = "1";
-	private static final Collection<String> ALLOWED_VERSIONS = Set.of(
-			PROTOCOL_VERSION,
-			NetworkRegistry.ABSENT.version(),
-			NetworkRegistry.ACCEPTVANILLA
-	);
-	
-	public static final SimpleChannel INSTANCE = NetworkRegistry.newSimpleChannel(
-			new ResourceLocation(FallingTree.MOD_ID, "main"),
-			() -> PROTOCOL_VERSION,
-			PROTOCOL_VERSION::equals,
-			v -> ALLOWED_VERSIONS.stream().anyMatch(v::equals)
-	);
+	public static final SimpleChannel INSTANCE = net.minecraftforge.network.ChannelBuilder.named(new ResourceLocation(FallingTree.MOD_ID, "main"))
+			.optionalClient()
+			.simpleChannel();
 	
 	private static final int CONFIGURATION_MESSAGE_ID = 1;
 	
@@ -43,27 +25,19 @@ public class ForgePacketHandler implements ClientPacketHandler, ServerPacketHand
 	
 	@Override
 	public void registerServer(){
-		INSTANCE.registerMessage(
-				CONFIGURATION_MESSAGE_ID,
-				ConfigurationPacket.class,
-				(packet, buf) -> mod.getPacketUtils().encodeConfigurationPacket(packet, new FriendlyByteBufWrapper(buf)),
-				buf -> mod.getPacketUtils().decodeConfigurationPacket(new FriendlyByteBufWrapper(buf)),
-				this::handleConfigurationPacket);
+		INSTANCE.messageBuilder(ConfigurationPacket.class, CONFIGURATION_MESSAGE_ID)
+				.decoder(buf -> ConfigurationPacket.read(new FriendlyByteBufWrapper(buf)))
+				.encoder((packet, buf) -> packet.write(new FriendlyByteBufWrapper(buf)))
+				.consumerMainThread(this::handleConfigurationPacket)
+				.add();
 	}
 	
 	@Override
 	public void registerClient(){
 	}
 	
-	public void handleConfigurationPacket(ConfigurationPacket configurationPacket, Supplier<NetworkEvent.Context> contextSupplier){
-		var context = contextSupplier.get();
+	public void handleConfigurationPacket(ConfigurationPacket configurationPacket, CustomPayloadEvent.Context context){
 		context.enqueueWork(() -> DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> mod.getPacketUtils().onClientConfigurationPacket(configurationPacket)));
 		context.setPacketHandled(true);
-	}
-	
-	@Override
-	public void onPlayerConnected(@NotNull IServerPlayer serverPlayer){
-		var target = PacketDistributor.PLAYER.with(() -> (ServerPlayer) serverPlayer.getRaw());
-		INSTANCE.send(target, mod.getPacketUtils().createConfigurationPacket());
 	}
 }
