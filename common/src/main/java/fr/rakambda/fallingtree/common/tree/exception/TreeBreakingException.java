76c831db166288cab69d1c2969883ca8ab15de20
Geolykt <admin@geolykt.de>
2023-10-28 16:23:01 +0200

    Improve performances by not using Exceptions for code control flow within BlockBreakListener (#615)
    * Do not use Exceptions for code control flow for BlockBreakListener
    
    * Implement nitpicks
    
    * Formatting & refactor shouldCancel & add enums values computed once
    
    ---------
    
    Co-authored-by: Thomas Couchoud <zerderr@gmail.com>
    Co-authored-by: Thomas <1688389+Rakambda@users.noreply.github.com>

--- common/src/main/java/fr/rakambda/fallingtree/common/tree/exception/TreeBreakingException.java
+++
@@ -1,7 +0,0 @@
-package fr.rakambda.fallingtree.common.tree.exception;
-
-public class TreeBreakingException extends Exception{
-	public TreeBreakingException(Throwable throwable){
-		super(throwable);
-	}
-}
--- forge/src/main/java/fr/rakambda/fallingtree/forge/event/BlockBreakListener.java
+++ forge/src/main/java/fr/rakambda/fallingtree/forge/event/BlockBreakListener.java
@@ -1,7 +1,6 @@
 package fr.rakambda.fallingtree.forge.event;
 
 import fr.rakambda.fallingtree.common.FallingTreeCommon;
-import fr.rakambda.fallingtree.common.tree.exception.*;
 import fr.rakambda.fallingtree.forge.common.wrapper.BlockPosWrapper;
 import fr.rakambda.fallingtree.forge.common.wrapper.LevelWrapper;
 import fr.rakambda.fallingtree.forge.common.wrapper.PlayerWrapper;
@@ -12,7 +11,6 @@ import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import org.jetbrains.annotations.NotNull;
-
 import javax.annotation.Nonnull;
 
 @RequiredArgsConstructor
@@ -55,21 +53,9 @@ public class BlockBreakListener{
 		var wrappedLevel = event.getLevel() instanceof ServerLevel serverLevel ? new ServerLevelWrapper(serverLevel) : new LevelWrapper(event.getLevel());
 		var wrappedPos = new BlockPosWrapper(event.getPos());
 		
-		try{
-			var result = mod.getTreeHandler().breakTree(wrappedLevel, wrappedPlayer, wrappedPos);
-			if(event.isCancelable()){
-				switch(result.breakMode()){
-					case INSTANTANEOUS, FALL_ITEM, FALL_BLOCK, FALL_ALL_BLOCK -> event.setCanceled(result.shouldCancel());
-					case SHIFT_DOWN -> event.setCanceled(true);
-				}
-			}
-		}
-		catch(TreeBreakingNotEnabledException | PlayerNotInRightState | TreeBreakingException | NoTreeFoundException | NotServerException ignored){
-		}
-		catch(ToolUseForcedException e){
-			if(event.isCancelable()){
-				event.setCanceled(true);
-			}
+		var result = mod.getTreeHandler().breakTree(wrappedLevel, wrappedPlayer, wrappedPos);
+		if(result.shouldCancel() && event.isCancelable()){
+			event.setCanceled(true);
 		}
 	}
 }
