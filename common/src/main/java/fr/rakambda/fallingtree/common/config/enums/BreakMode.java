8fc984686b3d11f0f2030299be89997857e37496
Thomas <1688389+Rakambda@users.noreply.github.com>
2023-10-28 16:59:40 +0200

    Improve performances by not using Exceptions for code control flow within BlockBreakListener (#615) (#622)
    * Do not use Exceptions for code control flow for BlockBreakListener
    
    * Implement nitpicks
    
    * Formatting & refactor shouldCancel & add enums values computed once
    
    ---------
    
    Co-authored-by: Geolykt <admin@geolykt.de>

--- common/src/main/java/fr/rakambda/fallingtree/common/config/enums/BreakMode.java
+++ common/src/main/java/fr/rakambda/fallingtree/common/config/enums/BreakMode.java
@@ -9,6 +9,9 @@ public enum BreakMode{
 	INSTANTANEOUS(true, true),
 	SHIFT_DOWN(false, false);
 	
+	@Getter
+	private final static BreakMode[] values = values();
+	
 	private final boolean checkLeavesAround;
 	private final boolean applySpeedMultiplier;
 }
