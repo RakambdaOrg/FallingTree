93482d9376e529994d0f2b616435eeb5445fdc3e
Thomas <1688389+Rakambda@users.noreply.github.com>
2023-10-28 16:37:48 +0200

    Improve performances by not using Exceptions for code control flow within BlockBreakListener (#615) (#619)
    * Do not use Exceptions for code control flow for BlockBreakListener
    
    * Implement nitpicks
    
    * Formatting & refactor shouldCancel & add enums values computed once
    
    ---------
    
    Co-authored-by: Geolykt <admin@geolykt.de>

--- common/src/main/java/fr/rakambda/fallingtree/common/tree/BreakTreeResult.java
+++ common/src/main/java/fr/rakambda/fallingtree/common/tree/BreakTreeResult.java
@@ -3,8 +3,22 @@ package fr.rakambda.fallingtree.common.tree;
 import fr.rakambda.fallingtree.common.config.enums.BreakMode;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Record that denotes that an attempt to break a tree with the given mode has
+ * succeeded. Failures are instead denoted as {@link AbortedResult}.
+ *
+ * @param shouldCancel Whether the event which triggered the query should be cancelled as a result of this result.
+ * @param breakMode    The mode with which the block was broken.
+ */
 public record BreakTreeResult(
 		boolean shouldCancel,
 		@NotNull BreakMode breakMode
-){
+) implements IBreakAttemptResult{
+	@Override
+	public boolean shouldCancel(){
+		return switch(breakMode()){
+			case INSTANTANEOUS, FALL_ITEM, FALL_BLOCK, FALL_ALL_BLOCK -> shouldCancel;
+			case SHIFT_DOWN -> true;
+		};
+	}
 }
