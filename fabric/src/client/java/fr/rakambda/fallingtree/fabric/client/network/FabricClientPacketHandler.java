dd200076f06b6539a14982412b156181e29e062c
Thomas Couchoud <1688389+Rakambda@users.noreply.github.com>
2024-05-02 21:35:04 +0200

    Fabric: fix packet encoding when joining server

--- fabric/src/client/java/fr/rakambda/fallingtree/fabric/client/network/FabricClientPacketHandler.java
+++ fabric/src/client/java/fr/rakambda/fallingtree/fabric/client/network/FabricClientPacketHandler.java
@@ -4,7 +4,6 @@ import fr.rakambda.fallingtree.common.FallingTreeCommon;
 import fr.rakambda.fallingtree.common.network.ClientPacketHandler;
 import fr.rakambda.fallingtree.fabric.network.FallingTreeConfigPacket;
 import net.fabricmc.fabric.api.client.networking.v1.ClientConfigurationNetworking;
-import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 
 public class FabricClientPacketHandler implements ClientPacketHandler{
 	private final FallingTreeCommon<?> mod;
@@ -15,9 +14,7 @@ public class FabricClientPacketHandler implements ClientPacketHandler{
 	
 	@Override
 	public void registerClient(){
-		PayloadTypeRegistry.configurationS2C().register(FallingTreeConfigPacket.TYPE, FallingTreeConfigPacket.CODEC);
-		ClientConfigurationNetworking.registerGlobalReceiver(FallingTreeConfigPacket.TYPE, (packet, sender) -> {
-			mod.getPacketUtils().onClientConfigurationPacket(packet.getPacket());
-		});
+		ClientConfigurationNetworking.registerGlobalReceiver(FallingTreeConfigPacket.TYPE,
+				(packet, sender) -> mod.getPacketUtils().onClientConfigurationPacket(packet.getPacket()));
 	}
 }
--- fabric/src/main/java/fr/rakambda/fallingtree/fabric/network/FabricServerPacketHandler.java
+++ fabric/src/main/java/fr/rakambda/fallingtree/fabric/network/FabricServerPacketHandler.java
@@ -4,6 +4,7 @@ import fr.rakambda.fallingtree.common.FallingTreeCommon;
 import fr.rakambda.fallingtree.common.network.ConfigurationPacket;
 import fr.rakambda.fallingtree.common.network.ServerPacketHandler;
 import lombok.RequiredArgsConstructor;
+import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.fabricmc.fabric.api.networking.v1.ServerConfigurationConnectionEvents;
 import net.fabricmc.fabric.api.networking.v1.ServerConfigurationNetworking;
 
@@ -13,6 +14,7 @@ public class FabricServerPacketHandler implements ServerPacketHandler{
 	
 	@Override
 	public void registerServer(){
+		PayloadTypeRegistry.configurationS2C().register(FallingTreeConfigPacket.TYPE, FallingTreeConfigPacket.CODEC);
 		ServerConfigurationConnectionEvents.CONFIGURE.register(((handler, server) -> {
 			var packet = ConfigurationPacket.get(mod.getConfiguration());
 			ServerConfigurationNetworking.send(handler, new FallingTreeConfigPacket(packet));
