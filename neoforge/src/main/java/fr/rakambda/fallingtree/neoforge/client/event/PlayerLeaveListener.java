fadc76e4d5ec6282493991ac2217b0db3f8b1111
Thomas Couchoud <1688389+Rakambda@users.noreply.github.com>
2024-05-25 19:18:01 +0200

    Fix neoforge startup on server

--- neoforge/src/main/java/fr/rakambda/fallingtree/neoforge/client/event/PlayerLeaveListener.java
+++ neoforge/src/main/java/fr/rakambda/fallingtree/neoforge/client/event/PlayerLeaveListener.java
@@ -2,8 +2,6 @@ package fr.rakambda.fallingtree.neoforge.client.event;
 
 import fr.rakambda.fallingtree.common.FallingTreeCommon;
 import lombok.RequiredArgsConstructor;
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.api.distmarker.OnlyIn;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.client.event.ClientPlayerNetworkEvent;
 import org.jetbrains.annotations.NotNull;
@@ -15,7 +13,6 @@ public class PlayerLeaveListener{
 	private final FallingTreeCommon<?> mod;
 	
 	@SubscribeEvent
-	@OnlyIn(Dist.CLIENT)
 	public void onPlayerLoggedOutEvent(@Nonnull ClientPlayerNetworkEvent.LoggingOut event){
 		mod.getPacketUtils().onClientDisconnect();
 	}
