f8daf34efa6558da8a134766f401d0c44ebeb533
Thomas Couchoud <1688389+Rakambda@users.noreply.github.com>
2025-03-27 21:18:37 +0100

    Disable cloth for forge

--- forge/src/main/java/fr/rakambda/fallingtree/forge/FallingTree.java
+++ forge/src/main/java/fr/rakambda/fallingtree/forge/FallingTree.java
@@ -1,15 +1,11 @@
 package fr.rakambda.fallingtree.forge;
 
-import fr.rakambda.fallingtree.common.FallingTreeCommon;
-import fr.rakambda.fallingtree.forge.client.cloth.ClothConfigHook;
 import fr.rakambda.fallingtree.forge.common.FallingTreeCommonsImpl;
 import lombok.Getter;
 import lombok.extern.log4j.Log4j2;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import java.lang.reflect.InvocationTargetException;
 
 @Log4j2
 @Mod(FallingTree.MOD_ID)
