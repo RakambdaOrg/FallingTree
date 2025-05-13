3381b02840f492ca69b47f55692d8b220682172f
Thomas <1688389+Rakambda@users.noreply.github.com>
2025-05-13 10:45:35 +0700

    Update fabric/src/client/java/fr/rakambda/fallingtree/fabric/client/cloth/ClothConfigHook.java
    Signed-off-by: Thomas <1688389+Rakambda@users.noreply.github.com>

--- fabric/src/client/java/fr/rakambda/fallingtree/fabric/client/cloth/ClothConfigHook.java
+++ fabric/src/client/java/fr/rakambda/fallingtree/fabric/client/cloth/ClothConfigHook.java
@@ -1,7 +1,6 @@
 package fr.rakambda.fallingtree.fabric.client.cloth;
 
 import com.google.common.collect.Lists;
-import com.terraformersmc.modmenu.util.mod.Mod;
 import fr.rakambda.fallingtree.common.FallingTreeCommon;
 import fr.rakambda.fallingtree.common.config.enums.AdjacentStopMode;
 import fr.rakambda.fallingtree.common.config.enums.BreakMode;
