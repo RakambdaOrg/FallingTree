8b5346a5f61d8ce6fde8b212fea85f24965e358d
Thomas <1688389+Rakambda@users.noreply.github.com>
2025-05-18 12:13:43 +0200

    Display a message in chat when VeinMiner is present, suggesting a command to avoid incompatibilities (#863)

--- forge/src/main/java/fr/rakambda/fallingtree/forge/FallingTree.java
+++ forge/src/main/java/fr/rakambda/fallingtree/forge/FallingTree.java
@@ -4,6 +4,7 @@ import fr.rakambda.fallingtree.forge.common.FallingTreeCommonsImpl;
 import lombok.Getter;
 import lombok.extern.log4j.Log4j2;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -27,6 +28,10 @@ public class FallingTree{
 		// 		log.error("Failed to hook into ClothConfig", e);
 		// 	}
 		// }
+		
+		if(ModList.get().isLoaded("veinminer")){
+			log.warn("VeinMiner is present, this may lead to incompatibilities. It is recommended to run the following command: /veinminer groups remove Wood");
+		}
 		
 		mod.registerForge(MinecraftForge.EVENT_BUS);
 	}
--- forge/src/main/java/fr/rakambda/fallingtree/forge/network/PlayerJoinListener.java
+++ forge/src/main/java/fr/rakambda/fallingtree/forge/network/PlayerJoinListener.java
@@ -1,12 +1,15 @@
 package fr.rakambda.fallingtree.forge.network;
 
 import fr.rakambda.fallingtree.common.FallingTreeCommon;
+import fr.rakambda.fallingtree.common.config.enums.NotificationMode;
 import fr.rakambda.fallingtree.common.network.ConfigurationPacket;
+import fr.rakambda.fallingtree.forge.common.wrapper.PlayerWrapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.network.PacketDistributor;
 import org.jetbrains.annotations.NotNull;
 import javax.annotation.Nonnull;
@@ -32,6 +35,10 @@ public class PlayerJoinListener{
 			}
 			else{
 				log.info("Player connected to a local world, not setting up proxy config");
+			}
+			
+			if(ModList.get().isLoaded("veinminer")){
+				new PlayerWrapper(event.getEntity()).sendMessage(mod.translate("chat.fallingtree.veinminer_incompatibility"), NotificationMode.CHAT);
 			}
 		}
 	}
