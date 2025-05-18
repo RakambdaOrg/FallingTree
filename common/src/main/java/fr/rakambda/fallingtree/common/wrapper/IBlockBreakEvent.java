b30256e3f2aa2285fdc63e99272f517885ad3cf3
Thomas <1688389+Rakambda@users.noreply.github.com>
2025-05-18 13:46:36 +0200

    Fix checking if block can be broken (to handle parcel like mods) (#865)

--- common/src/main/java/fr/rakambda/fallingtree/common/wrapper/IBlockBreakEvent.java
+++ common/src/main/java/fr/rakambda/fallingtree/common/wrapper/IBlockBreakEvent.java
@@ -1,4 +1,8 @@
 package fr.rakambda.fallingtree.common.wrapper;
 
+import org.jetbrains.annotations.NotNull;
+
 public interface IBlockBreakEvent extends IWrapper{
+	@NotNull
+	IBlockPos getBlockPos();
 }
