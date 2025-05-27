1a86fbf5f0f076fd9554f51cfbda3404add048c6
Thomas <1688389+Rakambda@users.noreply.github.com>
2025-05-27 21:06:42 +0200

    [Fabric] Fix world unjoinable without restarting minecraft if exiting while leaves are pending to be broken (#891)

--- fabric/src/main/java/fr/rakambda/fallingtree/fabric/common/FallingTreeCommonsImpl.java
+++ fabric/src/main/java/fr/rakambda/fallingtree/fabric/common/FallingTreeCommonsImpl.java
@@ -27,6 +27,7 @@ import fr.rakambda.fallingtree.fabric.network.FabricServerPacketHandler;
 import lombok.Getter;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
@@ -262,6 +263,7 @@ public class FallingTreeCommonsImpl extends FallingTreeCommon<Direction>{
 		getServerPacketHandler().registerServer();
 		
 		ServerTickEvents.END_SERVER_TICK.register(new LeafBreakingListener(this));
+		ServerWorldEvents.UNLOAD.register(new LeafBreakingListener(this));
 		PlayerBlockBreakEvents.BEFORE.register(new BlockBreakListener(this));
 		PlayerBlockBreakEvents.AFTER.register(new BlockBreakListener(this));
 		
