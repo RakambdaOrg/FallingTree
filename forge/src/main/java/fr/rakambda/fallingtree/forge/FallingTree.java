3458166e29c4fe2b2cfd88b7841995cd12382af8
Thomas Couchoud <1688389+Rakambda@users.noreply.github.com>
2025-06-19 20:43:37 +0200

    Code cleanup

--- forge/src/main/java/fr/rakambda/fallingtree/forge/FallingTree.java
+++ forge/src/main/java/fr/rakambda/fallingtree/forge/FallingTree.java
@@ -3,7 +3,6 @@ package fr.rakambda.fallingtree.forge;
 import fr.rakambda.fallingtree.forge.common.FallingTreeCommonsImpl;
 import lombok.Getter;
 import lombok.extern.log4j.Log4j2;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
