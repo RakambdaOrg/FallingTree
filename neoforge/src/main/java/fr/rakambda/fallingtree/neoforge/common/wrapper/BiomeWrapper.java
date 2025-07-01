d780345026ca02554a9b31099c79f7a04cdfd7d3
Thomas <1688389+Rakambda@users.noreply.github.com>
2025-07-01 11:56:40 +0200

    Add new positions fetcher to handle trees growing downwards + add new config `detectionModeBiomeOverride` that is accessible only through the config file (#917)

---
+++ neoforge/src/main/java/fr/rakambda/fallingtree/neoforge/common/wrapper/BiomeWrapper.java
@@ -0,0 +1,25 @@
+package fr.rakambda.fallingtree.neoforge.common.wrapper;
+
+import fr.rakambda.fallingtree.common.wrapper.IBiome;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import lombok.extern.log4j.Log4j2;
+import net.minecraft.core.Holder;
+import net.minecraft.world.level.biome.Biome;
+import org.jetbrains.annotations.NotNull;
+
+@RequiredArgsConstructor
+@ToString
+@Log4j2
+public class BiomeWrapper implements IBiome{
+	@NotNull
+	@Getter
+	private final Holder<Biome> raw;
+	
+	@Override
+	@NotNull
+	public String getId(){
+		return raw.getRegisteredName();
+	}
+}
