18f1b200e4c236371e1eeb1b599e4e7774c18fa7
Thomas <1688389+Rakambda@users.noreply.github.com>
2026-03-24 21:37:45 +0100

    Add NeoForge 26.1 support (#1057)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -25,9 +25,9 @@ pluginManagement {
     }
 }
 
-//plugins {
-//    id("org.gradle.toolchains.foojay-resolver-convention").version("1.0.0")
-//}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention").version("1.0.0")
+}
 
 val includeFabric: String by settings
 val includeForge: String by settings
