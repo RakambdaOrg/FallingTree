0a0f2f97c2c132820406435211a9614b0e766484
Thomas <1688389+Rakambda@users.noreply.github.com>
2026-04-24 17:14:05 +0200

    Update common/src/main/java/fr/rakambda/fallingtree/common/tree/breaking/FallingAnimationTreeBreakingHandler.java
    Signed-off-by: Thomas <1688389+Rakambda@users.noreply.github.com>

--- common/src/main/java/fr/rakambda/fallingtree/common/tree/breaking/FallingAnimationTreeBreakingHandler.java
+++ common/src/main/java/fr/rakambda/fallingtree/common/tree/breaking/FallingAnimationTreeBreakingHandler.java
@@ -81,7 +81,8 @@ public class FallingAnimationTreeBreakingHandler implements ITreeBreakingHandler
 		var lootHandler = new LootHandler(wantToBreakCount, mod.getConfiguration().getTrees().getTrunkLootPercentage());
 		var brokenCount = 0;
 		var breakablePartsLeft = wantToBreakCount;
-		for(var part : tree.getParts().stream().sorted(mod.getConfiguration().getTrees().getBreakOrder().getComparator()).toList()){
+        var breakableParts = tree.getParts().stream().sorted(mod.getConfiguration().getTrees().getBreakOrder().getComparator()).toList();
+		for(var part : breakableParts){
 			if(part.treePartType().isBreakable()){
 				if(breakablePartsLeft == 0){
 					break;
